f6ec3f3381d3c0c03413cba4ee244824d69ba8e5
Curtis <curtismensah48@gmail.com>
2020-08-28 02:57:17 +0000

    test release

---
+++ lmsvuefrontend/src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">Navbar</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> 
+  </form>
+</nav>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ lmsvuefrontend/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">LITT-LMS</a>
+  <form class="form-inline">
+    <li class="nav-item">
+        <a class="nav-link" href=".\signin.vue">Signin</a>
+      </li>
+  </form>
+</nav>
+</template>
+
+<script>
+export default {
+  name: '',
+  /**props: {
+    msg: String
+  } **/
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
