754725153740c4f6c65fc4e155340c55e5a06e1d
gloorya <gloriaalabi9@gmail.com>
2020-09-03 14:59:29 -0700

    newer files

--- lmsfrontend/src/router/index.js
+++ lmsfrontend/src/router/index.js
@@ -42,6 +42,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '/question',
+    name: 'Question',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Question.vue')
   }
   
 ]
