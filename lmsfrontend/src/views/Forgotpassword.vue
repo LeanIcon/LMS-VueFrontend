d4a4d5c61413e4ea60f5af9435c73fe83d25aec7
gloorya <gloriaalabi9@gmail.com>
2020-09-04 03:09:14 -0700

    password ui done

--- lmsfrontend/src/views/Forgotpassword.vue
+++ lmsfrontend/src/views/Forgotpassword.vue
@@ -1,8 +1,88 @@
 <template>
   <div class="forgotpassword">
+  <Footer></Footer>
     <router-link to="/forgotpassword"></router-link>
       <router-view/>
-      <div class="forms-container">
+      <div class="main_container">
+        <div>
+          <h2 class="container_title">Having trouble logging in?</h2>
+        </div>
+          <div>
+            <p class="paragraph">Please enter your LMS-id to reset your password</p>
+            <form class="form" action="post">
+              <input type="text" placeholder="email" class="input-container">
+                                
+            </form>
+          
+              <router-link :to = "{ name:'signin' }" exact class="btn signup_btn">Send request</router-link>
+          </div>
+        </div>
+
+
+  </div>    
+</template>
+<script>
+    // @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components :{
+    Footer
+  },
+}
+</script>
+<style>
+  body{
+    width: 100%;
+    overflow: hidden;
+  }
+  form input{
+    display: block;
+  }
+  .container_title{
+    font-family: 'Noto Sans TC', sans-serif, 'Source Sans Pro';
+    font-size: 40px;
+    font-weight: 500;
+    margin: auto;
+    margin-bottom: 20px;
+    margin-left: 20px;
+    text-align: center;
+  }
+  /* .forgotpassword{
+
+  } */
+  .main_container{
+    /* alignment: center; */
+    border: transparent;
+    margin-left: 20em;
+    margin-right: 20em;
+    margin-top: 5rem;
+
+  }
+  .paragraph{
+    text-align: center;
+  }
+  .input-container{
+    width:70%;
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+  .btn {
+    border: transparent;
+    background-color: rgba(195, 0, 12, 1); 
+    margin-left: 13rem;
+    margin-top: 5rem;
+    text-decoration: none;
+    width: 30%;
+    color: #fff;
+
+  }
+  .btn:hover {
+    color: #fff;
+
+  }
+      /* <div class="forms-container">
         <div class="reset-home">
           <div class="title">
             <h1>Having troubles logging in?</h1>
@@ -82,4 +162,4 @@ form > input {
   color: white;
 }
 
-</style>
+</style> */
