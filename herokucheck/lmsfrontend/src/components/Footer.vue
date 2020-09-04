11463db5ea9d27c0e6e4bbe31238d3e510577d22
Curtis <curtismensah48@gmail.com>
2020-09-04 02:01:11 +0000

    Add 'herokucheck/' from commit '7245bd8c096dc50271d82a963a860f8b4a14bd49'
    git-subtree-dir: herokucheck
    git-subtree-mainline: 7245bd8c096dc50271d82a963a860f8b4a14bd49
    git-subtree-split: 7245bd8c096dc50271d82a963a860f8b4a14bd49

---
+++ herokucheck/lmsfrontend/src/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+ <!-- <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">Navbar</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> 
+  </form>
+</nav>-->
+  <div class="footer row">
+         <div class="litt_logo col-4">
+                      <img src="../assets/gh_01_ek2.png" alt="">
+         </div>
+           <div class="footer-creds col-8">
+                 <a href="contactUs.html">Contact</a>
+                 <a href="Privacy.html">Privacy</a>
+                 <a href="tos.html">Terms of use</a>
+                 <a href="Accessibility.html">Accessibility</a>
+                 <a href="cookie.html">Cookie Prefrences</a>
+          </div>
+   </div>            
+</template>
+
+<script>
+export default {
+  name: 'footer row',
+  /**props: {
+    msg: String
+  }**/
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer{
+    background: rgba(190,30,45,255);
+    height: 15vh;
+    color: #fff;
+    place-content: center;
+    text-align: center;
+}
+
+.litt_logo img{
+    height: 120px;
+}
+
+.footer-creds{
+    margin: auto;
+}
+
+.footer-creds a{
+    text-decoration: none;
+    color: #fff;
+    margin-left: 20px;
+}</style>
---
+++ herokucheck/lmsfrontend/src/views/Courseregister.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="courseregister">
+        <div class="sidebar">
+            <Sidebar></Sidebar>
+        </div>
+        <div class="dashnav">
+          <Dashboardnavbar></Dashboardnavbar>
+        </div>
+        <main id="main" class="jumbotron">
+       <Section class="sec1">
+           <div class="h1-box"></div>
+           <h1>Business </h1>
+           <h1>Analysis</h1>
+           <div class="secText">
+               <p>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta culpa id ut dolorem accusantium placeat, quidem at nam, molestiae enim exercitationem earum harum cum beatae aliquam illum architecto quia. Earum?
+               </p>
+           </div>
+       </Section>
+       <Section class="sec2">
+           <div class="enroll">
+               <div class="enrollText">
+                   <p>You've enrolled on this course! </p>
+               </div>
+               <div class="enroll-btn">
+                  
+                <button class="btn btn-lg btn-primary"> Register</button>
+                      
+                   
+               </div>
+               <div class="enroll-list">
+                   <div class="enroll-icon"><i class="fas fa-list-alt"></i> 8 Lessons</div>
+                   <div class="enroll-icon"><i class="fab fa-youtube-square"></i> 50 Videos</div>
+                   <div class="enroll-icon"><i class="fa fa-clock"></i> 72 Hours</div>
+               </div>
+           </div>
+
+       </Section>
+       <Section class="sec1"></Section>
+    </main>
+    <Section class=" container card-box">
+        <hr>
+
+        <div class="card-deck mb-3 text-center">
+            <div class="card mb-4 shadow-sm cardd">
+                <div class="card-body">
+                    <button class="btn btn-lg btn-primary">Add new Course</button>
+                </div>
+            </div>
+            <div class="card mb-4 shadow-sm cardd">
+                <div class="card-body">
+                    <button class="btn btn-lg btn-primary">Add new Course</button>
+                </div>
+
+            </div>
+            <div class="card mb-4 shadow-sm cardd">
+                <div class="card-body">
+                    <button class="btn btn-lg btn-primary">Add new Course</button>
+                </div>
+            </div>
+        </div>
+    </Section>
+
+    </div>    
+</template>
+
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import Dashboardnavbar from '@/components/Dashboardnavbar.vue'
+export default {
+// @ is an alias to /src
+  name: 'Dashboard',
+  components:{
+    Sidebar,
+    Dashboardnavbar,
+  },
+}
+
+</script>
+
+<style scoped>
+.sidebar{
+  position: fixed;
+  z-index: 2;
+}
+
+.dashnav{
+  position: static;
+  z-index: 1;
+}
+
+#main{
+    background-color: #3B3B3B;
+    display:inline-block;
+    margin: 0 auto;
+    /* text-align: center;
+     */
+     
+}
+
+#main Section{
+    width: 50%;
+    /* display: inline-block; */
+    float: left;
+    margin: 0 auto;
+    padding:60px 0 0 100px;
+    text-justify: auto;
+}
+
+#main .sec1{
+    color:white;
+}
+
+#main h1{
+    font-weight: 900;
+    /* color: green; */
+
+}
+
+#main .secText{
+    text-align: justify;
+    /* margin: 200 0 0 0; */
+    
+
+}
+/* section 2 */
+.enroll{
+    margin: 0 auto;
+    width:300px;
+    padding:30px;
+    background-color: white;
+}
+
+.enroll-btn button{
+    background-color: #0E9CF5;
+    text-align: center;
+    color: white;
+    padding:10px;
+    width: 100%;
+    offset: 0;
+}
+
+.enroll-list{
+    padding: 20px 0 0 0;
+}
+
+.card-box{
+   padding: 60px;
+}
+.cardd{
+    /* height: 250;
+    width: 250; */
+    padding: 80px 0;
+}
+</style>
