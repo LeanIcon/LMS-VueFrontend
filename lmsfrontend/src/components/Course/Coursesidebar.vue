50750a003aed31b34470c3232259da214b39b672
Curtis <curtismensah48@gmail.com>
2020-09-21 18:10:21 +0000

    restructure file mods

---
+++ lmsfrontend/src/components/Course/Coursesidebar.vue
@@ -0,0 +1,126 @@
+<template>
+     <!--Course page sidebar-->
+            <div id="mySideBar" class="sidebar">
+                <div class="sidebar-header">
+                    <div class="header-text">
+                        <h3 class="bar-title">Course Overview</h3>
+                    </div>
+                </div>
+            
+            
+                <div class="mySideBarMenuItem"><a  data-toggle="collapse" data-target="#submenu-1">+  Course Introduction</a>
+                    <hr>
+                    <ul id="submenu-1" class="collapse side-nav">
+                        <li><a class="submenu-item">- Introduction</a></li>
+                        <li><a class="submenu-item">- Requirements</a></li>
+                    </ul>
+                </div>
+
+                <div class="mySideBarMenuItem"><a  data-toggle="collapse" data-target="#submenu-2">+  Market Analysis</a>
+                    <hr>
+                    <ul id="submenu-2" class="collapse side-nav">
+                        <li><a class="submenu-item">- Estimation</a></li>
+                        <li><a class="submenu-item">- Regression</a></li>
+                        <li><a class="submenu-item">- Linear Optimization</a></li>
+                        <li><a class="submenu-item">- Dynamic Optimization</a></li>
+                    </ul>
+                </div>
+
+                <div class="mySideBarMenuItem"><a  data-toggle="collapse" data-target="#submenu-3">+  Assessment</a>
+                    <hr>
+                    <ul id="submenu-3" class="collapse side-nav">
+                        <li><a class="submenu-item">- Quiz</a></li>
+                        <li><a class="submenu-item">- Course Recap</a></li>
+                        <li><a class="submenu-item">- Your Next Step</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+           
+
+
+</template>
+
+<script type="text/javascript">
+    export default {
+        name: "Coursesidebar",
+    };
+
+    (function () {
+        ('[data-toggle="tooltip"]').tooltip();
+        (".side-nav .collapse").on("hide.bs.collapse", function () {
+            (this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+        });
+        ('.side-nav .collapse').on("show.bs.collapse", function () {
+            (this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");
+        });
+    })
+</script>
+
+<style lang="scss" scoped>
+.sidebar{
+    height: 100%;
+    color: white;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #C3000C;
+    overflow-x: hidden;
+    transition: 0.5s;
+    font:lato;
+}
+.sidebar a{
+    /* padding: 8px 8px 8px 32px; */
+    text-decoration: none;
+    font-size: 16px;
+    color: white;
+    display: block;
+    transition: 0.3s;
+
+}
+
+.side-nav{
+    margin: 0 -2rem;
+}
+
+hr{
+    background-color: white;
+    opacity: 0.5;
+}
+
+.mySideBarMenuItem{
+    padding: 0 10px;
+}
+
+.collapse > li > a{
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.collapse > li, .side-nav > li{
+    list-style: none;
+}
+
+.side-nav li a:hover,
+.side-nav li a:focus {
+    outline: none;
+    color: rgba(195,0,12,1) !important;
+    background-color: #fff;
+}
+
+.side-nav>li>ul {
+    padding: 0;
+    border-bottom: 1px rgba(0,0,0,.3) solid;
+}
+
+.side-nav>li>ul>li>a {
+    display: block;
+    padding: 10px 15px 10px 38px;
+    text-decoration: none;
+    /*color: #999;*/
+    color: #fff;    
+}
+
+</style>
---
+++ lmsfrontend/src/components/Dashboard/Dashboardnavbar.vue
@@ -0,0 +1,194 @@
+<template>
+     <div class="dashboard-nav user-bar">
+      <div class="nav-area nav-list row">
+
+         <div class="search-icon nav-item col">
+            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none"
+               stroke="rgba(248,244,244,1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               class="feather feather-search">
+               <circle cx="11" cy="11" r="8"></circle>
+               <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+         </div>
+
+         <div class="right-shift row col">
+            <div class="bell-icon nav-item col icon dropdown">
+               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none"
+                  stroke="rgba(248,244,244,1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  class="feather feather-bell dropdown-toggle"  id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                  <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+               </svg>
+                              
+               <div class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="navbarDropdownMenuLink-333">
+                  <p class="dropdown-item">You have no new messages</p>
+               </div>
+            </div>
+
+            <div class="user-img col icon">
+               <img src="@/assets/user.png" alt="">
+            </div>
+
+            <div class="dropdown-icon nav-item dropdown col icon">
+               <img src="@/assets/dropdown.png" class="dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+               
+               <div class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="navbarDropdownMenuLink-333">
+                  <router-link :to = "{ name:'Dashboard' }" exact class="dropdown-item">My Courses</router-link>
+                  <a class="dropdown-item logout" v-on:click="created">Logout</a>
+               </div>
+            </div>
+
+         </div>
+      </div>
+   </div>
+</template>
+ 
+<script>   
+// import { store } from '../store/user';
+export default {
+   name: "Dashboardnavbar",
+   methods:{
+      // // When the user clicks on div, open the popup
+      // toggleNotification: function myFunction() {
+      //    var popup = document.getElementById("myPopup");
+      //    popup.classList.toggle("show");
+      // },
+      
+      created () {
+      this.$store.dispatch('userLogout')
+         .then(() => {
+            this.$router.push({ name: 'Signin' })
+         })
+      }
+  },
+}
+</script>
+
+<style scoped>
+.logout{
+   color: red;
+}
+
+.dropdown-menu{
+   margin-top: 1rem;
+   margin-right: -1rem;
+}
+
+.dashboard-nav{
+   background-color: rgb(255, 255, 255);
+   padding-left: 7rem;
+   position: fixed;
+   width: 100%;
+   top: 0px;
+   height: 52px;
+   z-index: 1;
+   box-shadow: rgb(207 198 198 / 50%) 0px 0px 8px 0px;
+}
+
+.nav-item > svg{
+    stroke: rgba(188,188,203,1);
+}
+
+.nav-list{
+    /* display: inline-flex; */
+    height: 52px;
+}
+
+.right-shift{
+    position: absolute;
+    /* padding-top: 50%, 0; */
+    right: 0px;
+    width: 10rem;
+    height: 52px;
+}
+
+/* .search-icon{
+    margin-left: 7rem;
+    margin-top: 10px;
+} */
+
+/* .bell-icon{
+    margin-right: 20px;
+    margin-top: 2px;
+} */
+
+.icon{
+   margin: auto 0px;
+   padding: 0px;
+}
+  
+.user-img > img {
+    height: 35px;
+    /* margin-right: 30px; */
+}
+
+.dropdown-icon > img{
+    height: 15px;
+    /* margin-right: 30px;     */
+}
+
+.search-icon:hover, .bell-icon:hover, .dropdown-icon:hover, .user-img>img:hover{
+   cursor: pointer;
+}
+
+
+/* ======================== */
+/* THE BELL NOTIFICATION */
+/* ========================== */
+.popup {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.popup .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  top: 48%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class - hide and show the popup */
+.popup .show {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s;
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;} 
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
+</style>
