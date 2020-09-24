2f2a09e77f2032ddb9ee56fac4bc20e57052a932
alexander agyeman <alexagyeman98@gmail.com>
2020-09-24 23:25:19 +0000

    Skills page modal

--- lmsfrontend/src/views/Skill.vue
+++ lmsfrontend/src/views/Skill.vue
@@ -9,15 +9,40 @@
     <div class="skillpage">
        <Section class=" container card-box">
         <hr>
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 20rem;">
             <img class="card-img-top" src="@/assets/download.png" alt="Card image cap">
             <div class="card-body shadow-sm cardd">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+              Launch demo modal
+            </button>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Start Test</button>
+                  </div>
+                </div>
+              </div>
+            </div>
             </div>
         </div>
-        <div class="card" style="width: 18rem;">
+
+        <!-- Card 2 -->
+        <div class="card" style="width: 20rem;">
             <img class="card-img-top" src="@/assets/logo.png" alt="Card image cap">
             <div class="card-body shadow-sm cardd">
               <h5 class="card-title">Card title</h5>
