acb7c7ecf13e8f470a1a28ac34c74d80fc434535
kris_wale <40746582+kristiankf@users.noreply.github.com>
2022-09-26 12:19:48 +0000

    created a new homepage called LmsHome.vue and refactored the nav bar abit

--- lmsfrontend/src/router/index.js
+++ lmsfrontend/src/router/index.js
@@ -9,204 +9,209 @@ import Homepage from '../views/Home/Classic.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-    {
-      path: "/home",
-      name: "Home",
-      component: Home,
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/dev/default",
-      name: "Home2",
-      component: Home2,
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/",
-      name: "Homepage",
-      component: Homepage,
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/individual",
-      name: "Individuals",
-      component: () => import("../views/Home/Individual.vue"),
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/overview",
-      name: "Overview",
-      component: Overview,
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/course/overview",
-      name: "CourseOverview",
-      component: CourseOverview,
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/signin",
-      name: "Signin",
-      component: () => import("../views/user/Signin.vue"),
-      meta: {
-        requiresAuth: false,
-        requiresLogin: false,
-      },
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: () => import("../views/user/Register.vue"),
-      meta: {
-        requiresAuth: false,
-      },
-    },
-    {
-      path: "/forgotpassword",
-      name: "Forgotpassword",
-      component: () => import("../views/user/Forgotpassword.vue"),
-    },
-    {
-      path: "/500",
-      name: "Error500",
-      component: () => import("../views/error/500.vue"),
-    },
-    {
-      path: "/reset",
-      name: "Resetpassword",
-      component: () => import("../views/user/Resetpassword.vue"),
-      props: route => ({ query: route.query.token })
-    },
-    {
-      path: "/accounts/activate/:header/:token_id",
-      name: "ActivateAccount",
-      component: () => import("../views/user/ActivateAccount.vue"),
-    },
-    {
-      path: "/dashboard",
-      name: "Dashboard",
-      component: () => import("../views/Dashboard.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresLogin: true,
-      },
-    },
-    {
-      path: "/help",
-      name: "Help",
-      component: () => import("../views/Help.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresLogin: true,
-      },
-    },
-    {path: "/courses",name: "Courseregister",component: () => import("../views/course/Courses.vue")},
-    {path: "/confirm",name: "Courseconfirm",component: () => import("../views/course/Courseconfirm.vue")},
-    
-    {
-      path: '/course',
-      component: CourseLesson,
-      meta: {requiresAuth: true, requiresLogin: true},
-      children: [
-        {path: "/courseintro",name: "Courseintro",component: () => import("../views/course/Courseintro.vue")},
-        {path: "/coursepage",name: "Coursepage",component: () => import("../views/course/Coursepage.vue")},
-        {path: "/course/lesson/:id",name: "Lessonpage",component: () => import("../views/course/Lessonpage.vue")},
-        
-      ]
+const routes = [
+  {
+    path: "/lmshome",
+    name: "LmsHome",
+    component: () => import("../views/Home/LmsHome.vue"),
   },
-    {
-      path: "/skill",
-      name: "Skill",
-      component: () => import("../views/Skill2.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/quiz/:slug",
-      name: "Question",
-      component: () => import("../views/Question.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  },
+  {
+    path: "/dev/default",
+    name: "Home2",
+    component: Home2,
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/test/",
-      name: "Test",
-      component: () => import("../views/quiz/quizPage.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  },
+  {
+    path: "/",
+    name: "Homepage",
+    component: Homepage,
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/quizdetail/:slug",
-      name: "QuizDetail",
-      component: () => import("../views/QuizDetail.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  },
+  {
+    path: "/individual",
+    name: "Individuals",
+    component: () => import("../views/Home/Individual.vue"),
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/quizoverview/:quiz_id",
-      name: "QuizOverview",
-      component: () => import("../views/QuizOverview.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  },
+  {
+    path: "/overview",
+    name: "Overview",
+    component: Overview,
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/results",
-      name: "QuizResults",
-      component: () => import("../views/quiz/QuizResults.vue"),
-      meta: {
-        requiresLogin: true,
-        requiresAuth: true,
-      }
+  },
+  {
+    path: "/course/overview",
+    name: "CourseOverview",
+    component: CourseOverview,
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/wrong-answers",
-      name: "WrongAnswers",
-      component: () => import("../views/quiz/WrongAnswers.vue"),
-      meta: {
-        requiresLogin: false,
-        requiresAuth: false,
-      }
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: () => import("../views/user/Signin.vue"),
+    meta: {
+      requiresAuth: false,
+      requiresLogin: false,
     },
-    {
-      path: "/editprofile",
-      name: "Editprofile",
-      component: () => import("../views/user/Editprofile.vue"),
-      meta: {
-        requiresLogin: false,
-        requiresAuth: false,
-      }
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/user/Register.vue"),
+    meta: {
+      requiresAuth: false,
     },
-  ];
+  },
+  {
+    path: "/forgotpassword",
+    name: "Forgotpassword",
+    component: () => import("../views/user/Forgotpassword.vue"),
+  },
+  {
+    path: "/500",
+    name: "Error500",
+    component: () => import("../views/error/500.vue"),
+  },
+  {
+    path: "/reset",
+    name: "Resetpassword",
+    component: () => import("../views/user/Resetpassword.vue"),
+    props: route => ({ query: route.query.token })
+  },
+  {
+    path: "/accounts/activate/:header/:token_id",
+    name: "ActivateAccount",
+    component: () => import("../views/user/ActivateAccount.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/Dashboard.vue"),
+    meta: {
+      requiresAuth: true,
+      requiresLogin: true,
+    },
+  },
+  {
+    path: "/help",
+    name: "Help",
+    component: () => import("../views/Help.vue"),
+    meta: {
+      requiresAuth: true,
+      requiresLogin: true,
+    },
+  },
+  { path: "/courses", name: "Courseregister", component: () => import("../views/course/Courses.vue") },
+  { path: "/confirm", name: "Courseconfirm", component: () => import("../views/course/Courseconfirm.vue") },
+
+  {
+    path: '/course',
+    component: CourseLesson,
+    meta: { requiresAuth: true, requiresLogin: true },
+    children: [
+      { path: "/courseintro", name: "Courseintro", component: () => import("../views/course/Courseintro.vue") },
+      { path: "/coursepage", name: "Coursepage", component: () => import("../views/course/Coursepage.vue") },
+      { path: "/course/lesson/:id", name: "Lessonpage", component: () => import("../views/course/Lessonpage.vue") },
+
+    ]
+  },
+  {
+    path: "/skill",
+    name: "Skill",
+    component: () => import("../views/Skill2.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/quiz/:slug",
+    name: "Question",
+    component: () => import("../views/Question.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/test/",
+    name: "Test",
+    component: () => import("../views/quiz/quizPage.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/quizdetail/:slug",
+    name: "QuizDetail",
+    component: () => import("../views/QuizDetail.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/quizoverview/:quiz_id",
+    name: "QuizOverview",
+    component: () => import("../views/QuizOverview.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/results",
+    name: "QuizResults",
+    component: () => import("../views/quiz/QuizResults.vue"),
+    meta: {
+      requiresLogin: true,
+      requiresAuth: true,
+    }
+  },
+  {
+    path: "/wrong-answers",
+    name: "WrongAnswers",
+    component: () => import("../views/quiz/WrongAnswers.vue"),
+    meta: {
+      requiresLogin: false,
+      requiresAuth: false,
+    }
+  },
+  {
+    path: "/editprofile",
+    name: "Editprofile",
+    component: () => import("../views/user/Editprofile.vue"),
+    meta: {
+      requiresLogin: false,
+      requiresAuth: false,
+    }
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
---
+++ lmsfrontend/src/views/Home/LmsHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div class="navigation">
+      <Navbar></Navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../../components/Navbar.vue";
+export default {
+  components: { Navbar },
+  name: "LmsHome",
+};
+</script>
+
+<style scoped>
+</style>
